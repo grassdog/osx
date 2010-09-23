25861e71b5af1f7d1483fabd617324fb35b47fc0
Ray Grasso <ray.grasso@gmail.com>
2010-09-23 14:05:07 +1000

    Update symlinks

--- setup-env.sh
+++ setup-env.sh
@@ -12,10 +12,11 @@ bash < <( curl http://rvm.beginrescueend.com/releases/rvm-install-head )
 sudo easy_install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz
 mkdir -p ~/bin/dotfiles/vim/tmp/backup
 mkdir -p ~/bin/dotfiles/vim/tmp/swap
-ln -sf ~/bin/dotfiles/vim/rcfiles/vimrc .vimrc
-ln -sf ~/bin/dotfiles/vim .vim                
-ln -sf ~/bin/dotfiles/zsh/zshenv .zshenv
-ln -sf ~/bin/dotfiles/git/gitconfig .gitconfig
-ln -sf ~/bin/dotfiles/git/gitignore_global .gitignore_global
-ln -sf ~/bin/dotfiles/gem/gemrc .gemrc
+ln -sf ~/bin/dotfiles/vim/rcfiles/vimrc ~/.vimrc
+ln -sf ~/bin/dotfiles/vim ~/.vim                
+ln -sf ~/bin/dotfiles/zsh/zshenv ~/.zshenv
+ln -sf ~/bin/dotfiles/git/gitconfig ~/.gitconfig
+ln -sf ~/bin/dotfiles/git/gitignore_global ~/.gitignore_global
+ln -sf ~/bin/dotfiles/gem/gemrc ~/.gemrc
+ln -sf ~/bin/dotfiles/zsh/dotzsh ~/.zsh
 

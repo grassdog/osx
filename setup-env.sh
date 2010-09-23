10b7a31546447b23bc1fadf56c35a260be573ea9
Ray Grasso <ray.grasso@gmail.com>
2010-09-23 10:28:25 +1000

    Add gemrc and setup script

---
+++ setup-env.sh
@@ -0,0 +1,8 @@
+
+ln -sf ~/bin/dotfiles/vim/rcfiles/vimrc .vimrc
+ln -sf ~/bin/dotfiles/vim .vim                
+ln -sf ~/bin/dotfiles/zsh/zshenv .zshenv
+ln -sf ~/bin/dotfiles/git/gitconfig .gitconfig
+ln -sf ~/bin/dotfiles/git/gitignore_global .gitignore_global
+ln -sf ~/bin/dotfiles/gem/gemrc .gemrc
+

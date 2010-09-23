73feb194bd57f1b30248196292967e5b5f8adc01
Ray Grasso <ray.grasso@gmail.com>
2010-09-23 14:02:07 +1000

    Update vimrc and plugin and other pieces

--- setup-env.sh
+++ setup-env.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 #TODO Add git and homebrew install stuff and pull of dotfiles from github
+
+ruby -e "$(curl -fsS http://gist.github.com/raw/323731/install_homebrew.rb)"
+brew install git
+brew install mysql
+brew install ctags
+
+bash < <( curl http://rvm.beginrescueend.com/releases/rvm-install-head )
+#TODO Install desired Rubys
+
+sudo easy_install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz
+mkdir -p ~/bin/dotfiles/vim/tmp/backup
+mkdir -p ~/bin/dotfiles/vim/tmp/swap
 ln -sf ~/bin/dotfiles/vim/rcfiles/vimrc .vimrc
 ln -sf ~/bin/dotfiles/vim .vim                
 ln -sf ~/bin/dotfiles/zsh/zshenv .zshenv

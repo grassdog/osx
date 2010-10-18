b1374a42c82277c4425696a276a39f12b48d811a
Ray Grasso <ray.grasso@gmail.com>
2010-10-18 12:03:57 +1100

    Update git diff tools

--- git-diff-wrapper.sh
+++ git-diff-wrapper.sh
@@ -3,5 +3,5 @@
 # diff is called by git with 7 parameters:
 # path old-file old-hex old-mode new-file new-hex new-mode
 
-opendiff "$2" "$5"
-
+#chdiff --wait "$2" "$5"
+opendiff "$2" "$5" -merge "$1"

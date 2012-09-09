2f748a2c6394279eb1bcd3252e22077032e37de4
Ray Grasso <ray.grasso@gmail.com>
2012-09-09 12:24:59 +1000

    Update setup-env.sh
    Put some notes in from my latest machine install

--- setup-env.sh
+++ setup-env.sh
@@ -1,29 +1,29 @@
 #!/bin/sh
 
-# TODO Install XCode
+# Install Apps
 
-# TODO set up keyboard defaults, repeat rate etc...
+# Must haves
+# XCode and command line tools
+# Mac vim, Total finder, iTerm2, Chrome, Dropbox, Alfred, Sparrow
 
-# TODO Set default shell to zsh
-# Bump up keyboard repeat rate
+# Rest
+# Skype, pester, adium,
+# httpclient, notational velocity alt, 
+# MSOffice, photoshop, VLC, Yojimbo, Virtual box, Sparrow, Skitch
+# Sequel pro 
 
-# Set up personal pro theme for Terminal
-# set background opacity to 100
-# set render bright as bold
-# set menlo bold, 12pt as font
-# set cursor color to green
-# set highlight color to blue
-# set size 100x43
-# set title active process name
-# set new pro as default
-# install fonts, Inconsolata
+# Set up keyboard defaults, repeat rate, caps lock as control etc...
+
+# Set default shell to zsh
+
+# Install solarized theme for iterm2
 
 # Get babushka
 #bash -c "`curl babushka.me/up`"
 
 # Remove glass dock
-defaults write com.apple.dock no-glass -boolean YES && killall Dock
-osascript -e 'tell app "Finder" to quit'
+#defaults write com.apple.dock no-glass -boolean YES && killall Dock
+#osascript -e 'tell app "Finder" to quit'
 
 # Hide ping dropdowns
 # Restore itunes arrows and set their defaul to filter my own library.
@@ -44,29 +44,16 @@ defaults write com.apple.iTunes invertStoreLinks 1
 curl https://raw.github.com/gist/810749/d4ff1627f3f1675be82ee29b44f362e56e93853e/DefaultKeyBinding.dict > ~/Library/KeyBindings/DefaultKeyBinding.dict
 
 # Install Homebrew
-ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 
-brew install git ctags wget tree vcprompt ack flip htop multimarkdown growlnotify
-#brew install mysql ghc mit-scheme node tmux
+brew install git ctags wget tree ack rbenv ruby-build
+brew install mysql ghc racket node
+#brew install flip htop multimarkdown
+
 #easy_install mercurial # or brew install pip && pip install mercurial
 
-# Install multimarkdown support and services
-
-# Apps to install
-# ruby mine, sequel pro, mac vim, git x, skype, pester, adium,
-# chrome, firefox, httpclient, dropbox, notational velocity alt, Alfred
-# textmate, MSOffice, acorn, photoshop, VLC, Yojimbo, virtual box, peepopen, Sparrow, Skitch
-# Total finder, iterm2
-
-# Install solarized theme for iterm2
-
-# TODO Change with rbenv
-bash < <( curl http://rvm.beginrescueend.com/releases/rvm-install-head )
-rvm install ruby-1.9.2
-rvm install ruby-1.8.7
-rvm use ruby-1.9.2 --default
-rvm use ruby-1.9.2@global
-gem install homesick ghost cheat bundler vagrant awesome_print key-installer
+rbenv install 1.9.3-p194
+gem install ghost bundler awesome_print pry
 
 # TODO Set up ssh keys
 
@@ -77,5 +64,4 @@ cd .dotfiles && rake install
 # Setup vim
 cd && git clone git@github.com:grassdog/vimfiles.git .vim
 cd .vim && rake setup
-
 

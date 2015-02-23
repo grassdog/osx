35a8f12db196234f936757052faa0ce0ea84e0bc
Ray Grasso <ray.grasso@gmail.com>
2015-02-23 19:34:27 +0800

    Add loco

---
+++ loco.rb
@@ -0,0 +1,7 @@
+dep "loco" do
+  requires "osx computer name set".with(computer_name: "loco", local_hostname: "loco"),
+           "all-apps",
+           "all-settings",
+           "all-fonts",
+           "dev-env"
+end
